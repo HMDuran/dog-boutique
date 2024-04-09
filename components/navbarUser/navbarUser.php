1ddcfec98ff395842916daf339f70e4db131a39e
HMDuran <hanahmaae.duran@gmail.com>
2024-04-09 11:43:55 +0800

    Add Navbar for User

---
+++ components/navbarUser/navbarUser.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href='/components/navbarUser/navbarUser.css'>
+
+<div class="navbar">
+    <div class="logo">
+        <img src="/public/assets/img/logo/logo.svg" alt="logo">
+    </div>
+
+    <div class="right-links">
+        <div class="icon"><img src="/public/assets/img/icons/account.png">
+        <div class="icon"><img src="/public/assets/img/icons/cart.png">
+        <div class="icon"><a href="/apps/views/home/home.php"><img src="/public/assets/img/icons/log-out.png"></a>
+    </div>
+</div>

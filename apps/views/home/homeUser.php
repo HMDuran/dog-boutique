de9ab71c3cb346b03d8db6a6f1560d73d469e9e3
HMDuran <hanahmaae.duran@gmail.com>
2024-04-09 11:44:17 +0800

    Add Home Page for User

---
+++ apps/views/home/homeUser.php
@@ -0,0 +1,8 @@
+<title>Home</title>
+<link rel="stylesheet" href='/public/styles/home/homeUser.css'>
+
+<main>
+   <?php
+      include '../../../components/navbarUser/navbarUser.php';
+   ?>
+</main>

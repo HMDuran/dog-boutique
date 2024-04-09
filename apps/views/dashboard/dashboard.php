9d180a61c56358f44cc917d31f592d0187017d2d
HMDuran <hanahmaae.duran@gmail.com>
2024-04-09 11:37:35 +0800

    Remove tailwind

---
+++ apps/views/dashboard/dashboard.php
@@ -0,0 +1,13 @@
+<title>Dashboard</title>
+<link rel="stylesheet" href="/public/styles/dashboard/dashboard.css"></link>
+
+<main>
+    <div class="container">
+        <div class="left">
+            <h1><Dashboard</h1>
+        </div>
+        <div class="right">
+            <h1><Dashboard</h1>
+        </div>
+    </div>
+</main>

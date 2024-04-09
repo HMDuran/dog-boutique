c39b6cd9c779f9de2478f517c6ec724cb1c02ac0
HMDuran <hanahmaae.duran@gmail.com>
2024-04-09 11:43:26 +0800

    Add Home page

---
+++ apps/views/home/home.php
@@ -0,0 +1,22 @@
+<title>Home</title>
+<link rel="stylesheet" href='/public/styles/home/home.css'>
+
+<main>
+   <?php
+      include $_SERVER['DOCUMENT_ROOT'] . '/components/navbar/navbar.php';
+   ?>
+
+   <div class="banner">
+      <div class="left">
+         <h2>Take Better Care of Your Dog.</h2>
+         <p>Discover tail-wagging fashion and purr-fect style for your best friend 
+            through our online shopping. Explore a great selection of clothes, 
+            cozy beds, playful toys, stylish collars, and more!</p>
+         <button>Shop Now</button>
+      </div>
+
+      <div class="right">
+         <img src="/public/assets/img/dogs/dogs.png">
+      </div>
+   </div>
+</main>

c465568c8dd8c5693a5b47701d2548a9bedf36d9
HMDuran <hanahmaae.duran@gmail.com>
2024-04-09 11:42:10 +0800

    Add Navbar

---
+++ components/navbar/navbar.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href='/components/navbar/navbar.css'>
+
+<div class="navbar">
+    <div class="logo">
+        <img src="/public/assets/img/logo/logo.svg" alt="logo">
+    </div>
+
+    <div class="right-links">
+        <a href="/apps/views/auth/login.php">Login</a>
+        <a href="/apps/views/auth/signup.php">Signup</a>
+    </div>
+</div>

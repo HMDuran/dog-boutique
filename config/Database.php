0a19affe4fa97acae86c5f290e95d232e3644b83
HMDuran <hanahmaae.duran@gmail.com>
2025-03-23 15:20:31 +0800

    feat(db): add .env support for database configuration

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,22 @@
 <?php
-class Database {
-    private $host = "localhost";
-    private $db_name = "dog_boutique";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+require_once __DIR__ . '/../vendor/autoload.php'; 
 
+use Dotenv\Dotenv;
+
+class Database {
+    private $conn;
     public function getConnection() {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../'); 
+        $dotenv->load();
+
+        $host = $_ENV['DB_HOST'];
+        $db_name = $_ENV['DB_NAME'];
+        $username = $_ENV['DB_USER'];
+        $password = $_ENV['DB_PASS'];
+
         $this->conn = null;
         try {   
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
